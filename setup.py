7b8e1d2718416322b8585b96a65cc13021990ac2
arkohut <39525455+arkohut@users.noreply.github.com>
2024-05-27 10:51:53 +0800

    chore: add extra info for setup

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='memos',
     version='0.1.0',
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="arkohut",
+    url="https://github.com/arkohut/memos",
     install_requires=[
         'fastapi',
         'uvicorn',
