b43453ae1328a009c9c562a19827ab7545ba6833
arkohut <39525455+arkohut@users.noreply.github.com>
2024-08-29 13:09:37 +0800

    feat(typesense): upgrade to 27

--- search-engine/run-docker.sh
+++ search-engine/run-docker.sh
@@ -3,7 +3,7 @@ export TYPESENSE_API_KEY=xyz
 mkdir "$(pwd)"/typesense-data
 
 docker run -d -p 8108:8108 \
-	-v"$(pwd)"/typesense-data:/data typesense/typesense:26.0 \
+	-v"$(pwd)"/typesense-data:/data typesense/typesense:27.0 \
 	--add-host=host.docker.internal:host-gateway \
 	--data-dir /data \
 	--api-key=$TYPESENSE_API_KEY \
