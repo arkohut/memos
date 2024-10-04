31838ac4a3db7302fbd88af9e574cab14e43a575
arkohut <39525455+arkohut@users.noreply.github.com>
2024-10-04 14:22:44 +0800

    fix(search): always restart

--- search-engine/run-docker.sh
+++ search-engine/run-docker.sh
@@ -3,6 +3,7 @@ export TYPESENSE_API_KEY=xyz
 mkdir "$(pwd)"/typesense-data
 
 docker run -d -p 8108:8108 \
+	--restart always \
 	-v"$(pwd)"/typesense-data:/data typesense/typesense:27.0 \
 	--add-host=host.docker.internal:host-gateway \
 	--data-dir /data \
